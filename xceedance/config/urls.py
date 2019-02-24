8ef0c6ce68524fdde0215de211e85eca8277d990
ritesh <ritesh.bisht@hindustantimes.com>
2019-02-24 17:03:32 +0530

    Add static url

--- xceedance/config/urls.py
+++ xceedance/config/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('users.urls', namespace='users')),
     url(r'^', include('features.urls', namespace='features')),
     url(r'^api-auth/', include('rest_framework.urls'))
-]
+] + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)
