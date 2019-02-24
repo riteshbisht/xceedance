4f6a3a5d025518fed82ee32fe719e59f74314ccd
ritesh <ritesh.bisht@hindustantimes.com>
2019-02-24 16:48:40 +0530

    changes in wsgi.py

--- xceedance/config/wsgi.py
+++ xceedance/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "featurerequest.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xceedance.config.settings")
 
 application = get_wsgi_application()
